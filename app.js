489013e9544042427b7b388f6d43f70d7c4107e6
markadillman <dillmanm@oregonstate.edu>
2017-06-03 00:27:06 -0400

    nw

--- app.js
+++ app.js
@@ -194,7 +194,8 @@ var findDocument = function(db,query,req,res,callback,initCoords,setname){
 	console.log(util.inspect(queryVals));
 	console.log('narrowed query');
 	console.log(util.inspect(narrowedQuery));
-	var newQuery = {narrowedQuery,{pw:0}};
+	var excludedFields = {pw : 0}
+	var newQuery = {narrowedQuery,excludedFields};
 	console.log("new query:");
 	console.log(util.inspect(newQuery));
 	collection.find(newQuery).toArray(function(err,docs){
